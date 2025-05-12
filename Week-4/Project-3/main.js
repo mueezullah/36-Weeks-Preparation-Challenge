37cf992857435865c85a5efaccbc9e0d396b2e3e
mueezullah <mueezullahip8@gmail.com>
2025-05-12 15:42:55 +0500

    ToDo App

---
+++ Week-4/Project-3/main.js
@@ -0,0 +1,22 @@
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.onclick = () => li.remove();
+
+  li.appendChild(delBtn);
+
+  document.getElementById("taskList").appendChild(li);
+  taskInput.value = "";
+}
